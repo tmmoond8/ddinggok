2511ba8b6a8c49578e7496c0dd18cc012b3481b5
Taemin Moon <tmmoond8@gmail.com>
2018-04-01 21:12:00 +0900

    - remove the needless files - set scss, add classnames

--- ddinggok-frontent/src/App.js
+++ ddinggok-frontent/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './test.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={cx('test')}>
+        메롱이야
       </div>
     );
   }
